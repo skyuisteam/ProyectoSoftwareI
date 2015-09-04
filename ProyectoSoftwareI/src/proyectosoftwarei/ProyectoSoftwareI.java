672385628bddb017a375dc13fb9c2d4fb09501b8
Yurs1 <www.notengocorreo@hotmail.es>
2015-09-03 19:50:55 -0500

    YURS
    [PRUEBA] - Agregar comentario e impresión por consola,

--- ProyectoSoftwareI/src/proyectosoftwarei/ProyectoSoftwareI.java
+++ ProyectoSoftwareI/src/proyectosoftwarei/ProyectoSoftwareI.java
@@ -16,6 +16,10 @@ public class ProyectoSoftwareI {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola Mundo");
     }
     
+    /*
+    Yurs
+    */
 }
